1512ff9bd03a473770b91af8c61720609aaec425
Hyller Bandeira <hyller.bandeira@gmail.com>
2019-05-30 13:10:37 -0300

    Remove sample code

--- src/index.js
+++ src/index.js
@@ -15,9 +15,6 @@ const app = express();
 // Get port from Heroku or set 3000 locally
 const port = process.env.PORT;  
 
-// Under maintance
-app.use(underMaintance);
-
 // Setting json body encode
 app.use(express.json())
 
@@ -27,32 +24,4 @@ app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('Server up on port ' + port);
-});
-
-const multer = require('multer');
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb)  {
-        const isPdf = file.originalname.endsWith('.pdf');
-        const isWordDocument = file.originalname.match('\.(doc|docx)$');
-
-        if (isWordDocument) {
-            cb(undefined, true);
-        } else {
-            cb(new Error('This isn\'t a Word file'));
-        }
-        // cb(new Error('Dummie error')); // Error
-        // cb(undefined, true); // Aprove
-        // cb(undefined, false); // Silent reject
-    }
-});
-
-app.post('/upload-test', upload.single('upload'), (req, res) => {
-    res.send();
-}, (error, req, res, next) => {
-    res.status(400)
-        .send({ error: error.message })
 });
